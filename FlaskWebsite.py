5428901e3088a6bacf0f384d5e248b97bea1c369
Mason Hartle <mhartle02@gmail.com>
2024-11-24 09:27:37 -0500

    Error validation and results table

--- FlaskWebsite.py
+++ FlaskWebsite.py
@@ -7,7 +7,6 @@ conn = sqlite3.connect('../BakingContest.db')
 #create cursor to exectute queries
 cur = conn.cursor()
 
-
 #Create USER table
 try:
     cur.execute("CREATE TABLE USER"
@@ -36,7 +35,6 @@ except Exception as e:
     print(e)
 
 app = Flask(__name__)
-
 @app.route('/')
 def home_page():
     return render_template('HomePage.html')
@@ -47,13 +45,20 @@ def enter_new():
 
 @app.route('/contestResults')
 def contest_results():
-    return render_template('ContestResults.html')
+    conn = sqlite3.connect('../BakingContest.db')
+    cur = conn.cursor()
+    cur.execute("SELECT Entry_Id, User_Id, Item_Name, "
+                "Excellent_Votes, Ok_Votes, Bad_Votes "
+                "FROM ENTRIES")
+    results = cur.fetchall()
+    conn.close()
+    return render_template('ContestResults.html', results=results)
 
 @app.route('/contestUsers')
 def contest_users():
-    conn = sqlite3.connect('example.db')
+    conn = sqlite3.connect('../BakingContest.db')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM USER')
+    cur.execute('SELECT Name, Age, Phone_Number, Security_Level, Login_Password FROM USER')
     users = cur.fetchall()
     conn.close()
     return render_template('ContestUsers.html', users=users)
@@ -65,17 +70,34 @@ def add_rec():
     phone_number = request.form['PhoneNumber']
     security_level = request.form['SecurityLevel']
     login_password = request.form['Password']
+    fields = [name, age, phone_number, security_level, login_password]
+    error_message = []
 
-    conn = sqlite3.connect('../BakingContest.db')
-    cur = conn.cursor()
-    cur.execute('INSERT INTO USER (Name, Age, Phone_Number, Security_Level, Login_Password) VALUES (? ,? ,?, ?, ?)', (name, age, phone_number, security_level, login_password))
-    conn.commit()
-    conn.close()
+    # Input validation
+    if not name or name.isspace():
+        error_message.append('You cannot enter an empty name.')
+    if age.isdigit() and 0 < int(age) < 121:
+        error_message.append('Age must be a whole number greater than 0 and less than 121.')
+    if not phone_number or phone_number.isspace():
+        error_message.append('You cannot enter an empty phone number.')
+    if not security_level or security_level.isspace():
+        error_message.append('You cannot enter an empty security level.')
+    if not login_password or login_password.isspace():
+        error_message.append('You cannot enter an empty login password.')
 
-    return render_template('AddRec.html')
+    if not error_message:
+        conn = sqlite3.connect('../BakingContest.db')
+        cur = conn.cursor()
+        cur.execute('INSERT INTO USER (Name, Age, Phone_Number, Security_Level, Login_Password) VALUES (?, ?, ?, ?, ?)',
+                    (name, age, phone_number, security_level, login_password))
+        conn.commit()
+        conn.close()
+        error_message.append('Record successfully added.')
+
+    for error in error_message:
+        print(error)
+
+    return render_template('AddRec.html', errors=error_message)
 
 if __name__ == '__main__':
-    app.run(debug=False)
-    t = cur.execute("SELECT * FROM USER")
-    for row in t:
-        print(row)
+    app.run(debug=True)
